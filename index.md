c372e4174926f9d0d378771d7d654e7e589fcde4
MURATA Mitsuharu <hikari.photon@gmail.com>
2020-04-05 11:12:37 +0900

    改善

--- index.md
+++ index.md
@@ -1,11 +1,20 @@
 ---
-layout: none
+layout: sars2
 ---
 
-# 山口県の SARS-CoV-2 感染者
+# 山口県の SARS-CoV-2 状況
 
-<div id="list">
-</div>
+<div id="sum"></div>
+<div id="map"></div>
+<div id="list"></div>
 
+<script src="https://himeyama.github.io/KeisanJS/era-1.1.0.js"></script>
 <script src="table.js"></script>
 <link rel="stylesheet" href="style.css">
+
+## 備考
+- 「感染源」は感染源である可能性が高いところ。
+- [新型コロナウイルス感染症の山口県内での発生について](https://www.pref.yamaguchi.lg.jp/cms/a15200/kansensyou/koronahassei.html) を参考。
+- 制作: [山口大学プログラミングサークル Ruby / Python 研究会](../)
+
+© 山口大学プログラミングサークル Ruby / Python 研究会
--- table.js
+++ table.js
@@ -9,23 +9,29 @@ function make_table(){
         let tr = document.createElement("tr")
         table.append(tr)
         let th
-        ths = ["番号", "公表日", "市町村", "年代", "性別", "国籍"]
+        ths = ["番号", "公表日", "市町村", "年代", "性別", "国籍", "感染源"]
         for(i in ths){
             th = document.createElement("th")
             tr.append(th)
             th.innerText = ths[i]
         }
-        for(i in info){
+        let citylist = {}
+        for(i in info["data"]){
             let tr = document.createElement("tr")
+            if(!citylist[info["data"][i]["city"]]){
+                citylist[info["data"][i]["city"]] = 0
+            }
+            citylist[info["data"][i]["city"]] += 1
             table.append(tr)
             let td
             tds = [
                 `${1+parseInt(i)}`,
-                info[i]["date"],
-                info[i]["city"],
-                info[i]["age"],
-                info[i]["sex"],
-                (!!info[i]["country"] ? info[i]["country"] : "")
+                (new Era(info["data"][i]["date"])).getWareki(),
+                info["data"][i]["city"],
+                info["data"][i]["age"],
+                info["data"][i]["sex"],
+                (info["data"][i]["country"] ? info["data"][i]["country"] : ""),
+                (info["data"][i]["from"] ? info["data"][i]["from"] : "")
             ]
             for(j in tds){
                 td = document.createElement("td")
@@ -33,6 +39,62 @@ function make_table(){
                 td.innerText = tds[j]
             }
         }
+        sum.innerHTML = `<span><ruby>現在<rt>${(new Era(info["header"]["date"])).getWareki()} 時点</rt></ruby> </span><span class="number">${info["data"].length}</span><span> 人</span>`
+        map_yamaguchi(citylist)
+    }
+}
+
+function map_yamaguchi(citylist){
+    let req = new XMLHttpRequest
+    req.open("GET", "yamaguchi.svg")
+    req.send()
+    req.onload = function(e){
+        map.innerHTML = req.responseText
+        for(i in Object.keys(citylist)){
+            let city = Object.keys(citylist)[i]
+            let n = citylist[city]
+            document.getElementById(city).style.fill = heatmap_red(n)
+        }
+        hover_city(citylist)
+    }
+}
+
+function heatmap_red(n){
+    reds = [
+        "#FFEBEE",
+        "#FFCDD2",
+        "#EF9A9A",
+        "#E57373",
+        "#EF5350",
+        "#F44336",
+        "#E53935",
+        "#D32F2F",
+        "#C62828",
+        "#B71C1C"
+    ]
+    return reds[parseInt(Math.log2(n))]
+}
+
+function hover_city(citylist){
+    let cities = document.getElementsByTagName("path")
+    let city_info = document.createElement("div")
+    city_info.id = "city_info"
+    document.body.getElementsByTagName("article")[0].append(city_info)
+    for(let i = 0; i < cities.length; i++){
+        cities[i].addEventListener('mousemove', e => {
+            let y = e.clientY
+            let x = e.clientX
+            city_info.style.top = `${y + 8}px`
+            city_info.style.left = `${x + 16}px`
+            city_info.innerText = `${e.target.id}\n${citylist[e.target.id] ? citylist[e.target.id] : 0}人`
+        })
+        document.body.addEventListener("mousemove", e => {
+            if(e.target.localName == "path"){
+                city_info.style.display = "block"
+            }else{
+                city_info.style.display = "none"
+            }
+        })
     }
 }
 
