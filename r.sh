52d504452eb732d6eb0db2b2485123867dec2e5d
Murata Mitsuharu <hikari.photon+mygit@gmail.com>
2020-07-25 08:09:02 +0900

    new

---
+++ r.sh
@@ -0,0 +1,2 @@
+rm *.csv
+wget https://yamaguchi-opendata.jp/ckan/dataset/f6e5cff9-ae43-4cd9-a398-085187277edf/resource/1a5f9bca-3216-45df-8a99-5c591df8f628/download/350001_yamaguchi_covid19_hospitalization.csv
